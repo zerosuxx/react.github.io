0ca82d087e0a9de9e8b8ecc0387ea9343952fff5
Gitlab Deployer <zerosuxx@gmail.com>
2020-06-24 23:11:36 +0000

    deploy

---
+++ precache-manifest.3e8abd4916ad799ca7a9fe9ebb6ab07a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b23deeb34fbf41352ae3fa1b3528c04b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa6a661639311f3a4333",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ce5267b335df5172fa96",
+    "url": "/static/js/2.89506c85.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.89506c85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa6a661639311f3a4333",
+    "url": "/static/js/main.22655879.chunk.js"
+  },
+  {
+    "revision": "981ccce08d60abfb0a3a",
+    "url": "/static/js/runtime-main.f02aa8f4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
